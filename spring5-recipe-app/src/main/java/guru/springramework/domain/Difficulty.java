4e52e152865284460ecca960ca85e90b6440eae7
Joachim Sogn <joachim.sogn@gmail.com>
2018-03-25 14:46:30 +0200

    Added ENUM type from java to say which difficulty a recipe has.

---
+++ spring5-recipe-app/src/main/java/guru/springramework/domain/Difficulty.java
@@ -0,0 +1,9 @@
+package guru.springramework.domain;
+
+/**
+ * Created by Joachim on 25/03/2018.
+ */
+public enum Difficulty {
+
+    EASY,MODERATE,HARD
+}
--- spring5-recipe-app/src/main/java/guru/springramework/domain/Recipe.java
+++ spring5-recipe-app/src/main/java/guru/springramework/domain/Recipe.java
@@ -30,6 +30,9 @@ public class Recipe {
     @Lob
     private Byte[] image;
 
+    @Enumerated(value = EnumType.STRING)
+    private Difficulty difficulty;
+
     @OneToOne(cascade = CascadeType.ALL)
     private Notes notes;
 
@@ -111,5 +114,21 @@ public class Recipe {
 
     public void setNotes(Notes notes) {
         this.notes = notes;
+    }
+
+    public Set<Ingredient> getIngredients() {
+        return ingredients;
+    }
+
+    public void setIngredients(Set<Ingredient> ingredients) {
+        this.ingredients = ingredients;
+    }
+
+    public Difficulty getDifficulty() {
+        return difficulty;
+    }
+
+    public void setDifficulty(Difficulty difficulty) {
+        this.difficulty = difficulty;
     }
 }
